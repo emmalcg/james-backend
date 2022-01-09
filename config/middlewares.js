f0cb1064d93f558a478e93e8716c7df8eaa221d2
emma <emmalcgreen@gmail.com>
2022-01-09 12:08:33 -0500

    trying to get image to show in strapi admin

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,5 +1,20 @@
 module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDeaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      }
+    },
+  },
+  'strapi::errors',
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',

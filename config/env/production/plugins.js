2aa78588bcff994526be893f41d578f7f094a040
emma <emmalcgreen@gmail.com>
2022-01-08 21:11:53 -0500

    adding graph ql

---
+++ config/env/production/plugins.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
+  },
+});
